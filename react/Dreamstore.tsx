fd5406ee3445173d2d50bd020e509ef5f96bb74c
Gabriel Galc <gabriel.galc@vtex.com.br>
2018-12-06 01:26:22 -0200

    Style dreamstore docs page

--- react/Dreamstore.tsx
+++ react/Dreamstore.tsx
@@ -13,8 +13,26 @@ export default class Dreamstore extends Component {
           <meta name="theme-color" content="#F71963" />
           <link rel="icon" href={favicon} />
         </Helmet>
-        <div>
-          <h1>Hello, Dreamstore!</h1>
+        <div className="w-40-ns w-90 center flex flex-column vh-100-ns justify-center">
+          <div>
+            <div className="t-heading-1 pb4 lh-solid pt7">Dreamstore Docs</div>
+            <div className="t-heading-3 fw3 pb6 pt4 lh-title">Want to create a store using VTEX IO? Start reading those contents below:</div>
+          </div>
+          <div className="pv7 bt bb b--muted-3 t-heading-2-ns fw3-ns t-heading-3 fw3 pointer dim">
+            <a className="c-on-base"><span className="c-emphasis">1.</span> Dreamstore Announcement</a>
+          </div>
+          <div className="pv7 bb b--muted-3 t-heading-2-ns fw3-ns t-heading-3 fw3 pointer dim">
+            <a className="c-on-base"><span className="c-emphasis">2.</span> Dreamstore Getting Started</a>
+          </div>
+          <div className="pv7 bb b--muted-3 t-heading-2-ns fw3-ns t-heading-3 fw3 pointer dim">
+            <a className="c-on-base"><span className="c-emphasis">3.</span> Style Builder</a>
+          </div>
+          <div className="pv7 bb b--muted-3 t-heading-2-ns fw3-ns t-heading-3 fw3 pointer dim">
+            <a className="c-on-base"><span className="c-emphasis">4.</span> Store Builder</a>
+          </div>
+          <div className="pv7 bb b--muted-3 t-heading-2-ns fw3-ns t-heading-3 fw3 pointer dim">
+            <a className="c-on-base"><span className="c-emphasis">5.</span> Contributing.md</a>
+          </div>
         </div>
       </main>
     )
