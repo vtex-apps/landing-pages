27ec7058bc74aad77d9b8322aefdeed29c4ea63c
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-04-20 19:36:14 -0300

    Add new type definitions

---
+++ react/typings/vtex.render-runtime.d.ts
@@ -0,0 +1,25 @@
+/* Typings for `render-runtime` */
+declare module 'vtex.render-runtime' {
+  import { Component, ReactElement } from 'react'
+
+  export interface RenderContextProps {
+    runtime: {
+      account: string
+    }
+  }
+
+  export const Link: ReactElement
+  export const NoSSR: ReactElement
+  export const RenderContextConsumer: ReactElement
+  export const canUseDOM: boolean
+  export const withRuntimeContext: ReactElement
+  export const Helmet: ReactElement
+  export const useRuntime: () => {
+    culture: {
+      locale: string
+    }
+    emitter: {
+      emit: (eventName: string, eventId: string) => void
+    }
+  }
+}
