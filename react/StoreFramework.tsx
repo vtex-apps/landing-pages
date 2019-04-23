13c53af0870f6d801b7724c905bc5aa15dc7f94f
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-04-23 10:13:58 -0300

    I18n for StoreFramework landing

--- react/StoreFramework.tsx
+++ react/StoreFramework.tsx
@@ -1,15 +1,18 @@
 import React, { Component, Fragment } from 'react'
+import { injectIntl, InjectedIntlProps } from 'react-intl'
 import { Helmet } from 'vtex.render-runtime'
 
 import favicon from './images/favicon.png'
 
-export default class StoreFramework extends Component {
+class StoreFramework extends Component<InjectedIntlProps> {
   public render() {
+    const { intl } = this.props
+
     return (
       <Fragment>
         <Helmet>
           <title>
-            VTEX Store Framework - The next generation store framework
+            {intl.formatMessage({ id: 'store-framework.page-title' })}
           </title>
           <meta
             name="description"
@@ -22,37 +25,46 @@ export default class StoreFramework extends Component {
           <div className="w-40-ns w-90 center flex flex-column vh-100-ns justify-center">
             <div className="mt9">
               <div className="t-heading-1 c-on-base pb4 lh-solid pt7">
-                Store Framework Docs
+                {intl.formatMessage({ id: 'store-framework.title' })}
               </div>
               <div className="t-heading-3 c-on-base fw3 pb6 pt4 lh-title">
-                Want to create a store using VTEX IO? Start reading those
-                contents below:
+                {intl.formatMessage({ id: 'store-framework.subtitle' })}
               </div>
             </div>
             <div className="pv7 bt bb b--muted-3 t-heading-2-ns fw3-ns t-heading-3 fw3">
               <a className="c-on-base link">
-                <span className="c-emphasis">1.</span> Store Framework
-                Announcement
-                <span className="c-muted-3"> [SOON]</span>
+                <span className="c-emphasis">1.</span>{' '}
+                {intl.formatMessage({ id: 'store-framework.announcement' })}
+                <span className="c-muted-3">
+                  {' '}
+                  [{intl.formatMessage({ id: 'store-framework.soon' })}]
+                </span>
               </a>
             </div>
             <div className="pv7 bb b--muted-3 t-heading-2-ns fw3-ns t-heading-3 fw3">
               <a className="c-on-base link">
-                <span className="c-emphasis">2.</span> Store Framework Getting
-                Started
-                <span className="c-muted-3"> [SOON]</span>
+                <span className="c-emphasis">2.</span>{' '}
+                {intl.formatMessage({ id: 'store-framework.getting-started' })}
+                <span className="c-muted-3">
+                  {' '}
+                  [{intl.formatMessage({ id: 'store-framework.soon' })}]
+                </span>
               </a>
             </div>
             <div className="pv7 bb b--muted-3 t-heading-2-ns fw3-ns t-heading-3 fw3">
               <a className="c-on-base link">
-                <span className="c-emphasis">3.</span> Store Builder
-                <span className="c-muted-3"> [SOON]</span>
+                <span className="c-emphasis">3.</span>{' '}
+                {intl.formatMessage({ id: 'store-framework.store-builder' })}
+                <span className="c-muted-3">
+                  {' '}
+                  [{intl.formatMessage({ id: 'store-framework.soon' })}]
+                </span>
               </a>
             </div>
             <div className="pv7 bb b--muted-3 t-heading-2-ns fw3-ns t-heading-3 fw3 pointer dim">
               <a className="c-on-base link" href="https://styleguide.vtex.com">
-                <span className="c-emphasis">4.</span> VTEX Tachyons and
-                Styleguide
+                <span className="c-emphasis">4.</span>{' '}
+                {intl.formatMessage({ id: 'store-framework.styles' })}
               </a>
             </div>
             <div className="pv7 mb7 bb b--muted-3 t-heading-2-ns fw3-ns t-heading-3 fw3 pointer dim">
@@ -60,7 +72,8 @@ export default class StoreFramework extends Component {
                 className="c-on-base link"
                 href="https://github.com/vtex-apps/awesome-io"
               >
-                <span className="c-emphasis">5.</span> Contributing.md
+                <span className="c-emphasis">5.</span>{' '}
+                {intl.formatMessage({ id: 'store-framework.contribute' })}
               </a>
             </div>
           </div>
@@ -69,3 +82,5 @@ export default class StoreFramework extends Component {
     )
   }
 }
+
+export default injectIntl(StoreFramework)
