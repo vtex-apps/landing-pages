bd28002faf38fc3e28176f21372c8fe102f7d89f
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-04-24 19:21:15 -0300

    First iteration for the Features component

--- react/Features.tsx
+++ react/Features.tsx
@@ -5,19 +5,45 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import favicon from './images/favicon.png'
 
+import { storeComponents, storePixel } from './FeaturesData'
+
 const FeaturesDisplay: FunctionComponent = () => {
   return (
     <Fragment>
       <Helmet>
-        <title>Store Framework Features</title>
+        <title>Store Framework</title>
         <meta name="theme-color" content="#F71963" />
         <link rel="icon" href={favicon} />
       </Helmet>
       <Navbar />
-      <main className="w-100 pv10">
-        <h1 className="c-on-base t-heading-1 w-90 center">
-          Store Framework Features
-        </h1>
+      <main className="w-100 pv10 bg-base--inverted">
+        <h1 className="c-emphasis t-heading-1 w-90 center">Store Framework</h1>
+        <section className="flex flex-column w-90 center">
+          <h2 className="c-emphasis t-heading-2">Store Component Apps</h2>
+          <ul className="t-body list">
+            {storeComponents.map(component => (
+              <li className="mv7 bb b--muted-2">
+                <p>{component.name}</p>
+                <p>
+                  <a className="c-muted-2 link" href={component.docs}>
+                    {component.docs}
+                  </a>
+                </p>
+              </li>
+            ))}
+          </ul>
+        </section>
+        <section className="flex flex-column w-90 center">
+          <h2 className="c-emphasis t-heading-2">Store Pixel Apps</h2>
+          <ul className="t-body list">
+            {storePixel.map(app => (
+              <li className="mv7 bb b--muted-2">
+                <p>{app.name}</p>
+                <p className="c-muted-2">{app.docs}</p>
+              </li>
+            ))}
+          </ul>
+        </section>
       </main>
       <Footer />
     </Fragment>
---
+++ react/FeaturesData.ts
@@ -0,0 +1,77 @@
+export const storeComponents = [
+  {
+    name: 'Store Components',
+    docs: 'https://github.com/vtex-apps/store-components',
+  },
+  {
+    name: 'Store Header',
+    docs: 'https://github.com/vtex-apps/store-header',
+  },
+  {
+    name: 'Store Footer',
+    docs: 'https://github.com/vtex-apps/store-footer',
+  },
+  {
+    name: 'Shelf',
+    docs: 'https://github.com/vtex-apps/shelf',
+  },
+  {
+    name: 'Carousel',
+    docs: 'https://github.com/vtex-apps/carousel',
+  },
+  {
+    name: 'Menu',
+    docs: 'https://github.com/vtex-apps/menu',
+  },
+  {
+    name: 'Minicart',
+    docs: 'https://github.com/vtex-apps/minicart',
+  },
+  {
+    name: 'Telemarketing',
+    docs: 'https://github.com/vtex-apps/telemarketing',
+  },
+  {
+    name: 'Login',
+    docs: 'https://github.com/vtex-apps/login',
+  },
+  {
+    name: 'Category Menu',
+    docs: 'https://github.com/vtex-apps/category-menu',
+  },
+  {
+    name: 'Breadcrumb',
+    docs: 'https://github.com/vtex-apps/breadcrumb',
+  },
+  {
+    name: 'Search Result',
+    docs: 'https://github.com/vtex-apps/search-result',
+  },
+  {
+    name: 'Product Details',
+    docs: 'https://github.com/vtex-apps/product-details',
+  },
+  {
+    name: 'Wishlist',
+    docs: 'https://github.com/vtex-apps/wishlist',
+  },
+  {
+    name: 'Order Placed',
+    docs: 'https://github.com/vtex-apps/order-placed',
+  },
+]
+
+export const storePixel = [
+  {
+    name: 'Facebook Pixel',
+    docs: 'https://github.com/vtex-apps/facebook-pixel',
+  },
+  {
+    name: 'Google Adwords',
+    docs: 'https://github.com/vtex-apps/google-adwords',
+  },
+  {
+    name: 'Google Tag Manager',
+    docs: 'https://github.com/vtex-apps/google-tag-manager',
+  },
+]
