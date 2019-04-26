c04f47bb8c87c00bab226827b246e2e944292949
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-04-26 18:58:08 -0300

    Add first feature list version

--- react/Features.tsx
+++ react/Features.tsx
@@ -1,11 +1,13 @@
 import React, { Fragment, FunctionComponent } from 'react'
+import { FormattedMessage } from 'react-intl'
 import { Helmet } from 'vtex.render-runtime'
+import { IconCheck, IconDeny } from 'vtex.styleguide'
 
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import favicon from './images/favicon.png'
 
-import { storeComponents, storePixel } from './FeaturesData'
+import { storeComponents, storePixel, features } from './FeaturesData'
 
 const FeaturesDisplay: FunctionComponent = () => {
   return (
@@ -17,13 +19,67 @@ const FeaturesDisplay: FunctionComponent = () => {
       </Helmet>
       <Navbar />
       <main className="w-100 pv10 bg-base--inverted">
-        <h1 className="c-emphasis t-heading-1 w-90 center">Store Framework</h1>
+        <h1 className="c-emphasis t-heading-1 w-90 center">
+          Store Framework Features
+        </h1>
+        <section className="flex flex-column w-90 center">
+          <h2 className="c-emphasis t-heading-2">VTEX CMS Features</h2>
+          <ul className="t-body list">
+            <li className="pv5 bb b--muted-2 flex justify-between items-center">
+              <div>
+                <p>Feature</p>
+                <p className="c-muted-2">Description</p>
+              </div>
+              <div className="flex justify-around w-40">
+                <p>Old CMS</p>
+                <p>New CMS</p>
+              </div>
+            </li>
+            {features.map(feature => (
+              <li className="pv5 bb b--muted-2 flex justify-between items-center">
+                <div>
+                  <p>
+                    <FormattedMessage id={`cms-features.${feature.id}`} />
+                  </p>
+                  <p className="c-muted-2">
+                    <FormattedMessage
+                      id={`cms-features.${feature.id}.description`}
+                    />
+                  </p>
+                </div>
+                <div className="flex justify-around w-40">
+                  {feature.old ? (
+                    <div className="c-success">
+                      <IconCheck size="25" />
+                    </div>
+                  ) : (
+                    <div className="c-danger">
+                      <IconDeny size="20" />
+                    </div>
+                  )}
+                  {feature.new ? (
+                    <div className="c-success">
+                      <IconCheck size="25" />
+                    </div>
+                  ) : (
+                    <div className="c-danger">
+                      <IconDeny size="20" />
+                    </div>
+                  )}
+                </div>
+              </li>
+            ))}
+          </ul>
+        </section>
         <section className="flex flex-column w-90 center">
           <h2 className="c-emphasis t-heading-2">Store Component Apps</h2>
           <ul className="t-body list">
             {storeComponents.map(component => (
               <li className="mv7 bb b--muted-2">
-                <p>{component.name}</p>
+                <p>
+                  {component.name}{' '}
+                  {component.construction && '[Under Construction] 🚧'}
+                </p>
                 <p>
                   <a className="c-muted-2 link" href={component.docs}>
                     {component.docs}
--- react/FeaturesData.ts
+++ react/FeaturesData.ts
@@ -54,10 +54,12 @@ export const storeComponents = [
   {
     name: 'Wishlist',
     docs: 'https://github.com/vtex-apps/wishlist',
+    construction: true,
   },
   {
     name: 'Order Placed',
     docs: 'https://github.com/vtex-apps/order-placed',
+    construction: true,
   },
 ]
 
@@ -73,5 +75,208 @@ export const storePixel = [
   {
     name: 'Google Tag Manager',
     docs: 'https://github.com/vtex-apps/google-tag-manager',
+  },
+]
+
+export const features = [
+  {
+    id: 'own-html',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'javascript',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'closed-websites',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'view-parts',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'pwa',
+    old: true,
+    new: true,
+  },
+  {
+    id: 'workspaces',
+    old: true,
+    new: true,
+  },
+  {
+    id: 'multi-websites',
+    old: true,
+    new: true,
+  },
+  {
+    id: 'apps',
+    old: true,
+    new: true,
+  },
+  {
+    id: 'themes',
+    old: true,
+    new: true,
+  },
+  {
+    id: 'multivendors',
+    old: false,
+    new: true,
+  },
+  {
+    id: 'assembly-options',
+    old: true,
+    new: true,
+  },
+  {
+    id: 'bread-crumb',
+    old: true,
+    new: true,
+  },
+  {
+    id: 'full-text-search-box',
+    old: true,
+    new: true,
+  },
+  {
+    id: 'store-flag-locator',
+    old: false,
+    new: true,
+  },
+  {
+    id: 'welcome-message',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'meta',
+    old: true,
+    new: true,
+  },
+  {
+    id: 'department-links',
+    old: true,
+    new: true,
+  },
+  {
+    id: 'department-navigator',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'search-title',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'product-query-string',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'product-quick-view',
+    old: false,
+    new: false,
+  },
+  {
+    id: 'product-page-title',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'product-name',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'product-brand',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'product-image',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'product-reference',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'product-price',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'sku-reference',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'sku-price',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'stock-keeping',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'sku-rich-selection',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'sku-selection',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'other-payment-methods',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'delivery',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'shipping-value',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'stock-keeping-reward-values',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'stock-keeping-unit-service',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'buy-together',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'product-description',
+    old: true,
+    new: false,
+  },
+  {
+    id: 'product-description-short',
+    old: true,
+    new: false,
   },
 ]
