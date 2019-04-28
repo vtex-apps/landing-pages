adef9837af59a994b430a5c68136bd6134aa54b5
Gabriel Galc <gabriel.galc@vtex.com.br>
2019-04-28 17:30:06 -0300

    Improve FAQ layout

--- react/Features.tsx
+++ react/Features.tsx
@@ -13,21 +13,28 @@ const FeaturesDisplay: FunctionComponent = () => {
   return (
     <Fragment>
       <Helmet>
-        <title>Store Framework</title>
+        <title>Store</title>
         <meta name="theme-color" content="#F71963" />
         <link rel="icon" href={favicon} />
       </Helmet>
       <Navbar />
       <main className="w-100 pv10 bg-base--inverted">
-        <h1 className="c-emphasis t-heading-1 w-90 center">Store Features</h1>
-        <p className="w-90 center">
-          CMS Comparison |{' '}
-          <a href="#store-component-apps">Store Component Apps</a> |{' '}
-          <a href="#store-pixel-apps">Store Pixel Apps</a>
-        </p>
-        <section className="flex flex-column w-90 center">
-          <h2 className="c-emphasis t-heading-2">VTEX CMS Comparison</h2>
-          <ul className="t-body list">
+        <div className="flex-ns items-baseline w-90 w-80-ns center mb8">
+          <h1 className="c-base t-heading-1 pr7">Store Features</h1>
+          <nav>
+            <ul className="flex-ns list t-heading-5 pl0">
+              <li className="pr6 pb4 pb0-ns"><a href="#vtex-oms-comparison" className="no-underline c-emphasis dim">CMS Comparison</a></li>
+              <li className="pr6 pb4 pb0-ns"><a href="#store-component-apps" className="no-underline c-emphasis dim">Store Component Apps</a></li>
+              <li><a href="#store-pixel-apps" className="no-underline c-emphasis dim">Store Pixel Apps</a></li>
+            </ul>
+          </nav>
+        </div>
+        <section
+          id="vtex-oms-comparison"
+          className="flex flex-column w-90 w-80-ns center"
+          >
+          <h2 className="c-base t-heading-3 fw3 mb0 mt4">VTEX CMS Comparison</h2>
+          <ul className="t-body list pl0 mt0">
             <li className="pv5 bb b--muted-2 flex justify-between items-center">
               <div>
                 <p>Feature</p>
@@ -76,10 +83,10 @@ const FeaturesDisplay: FunctionComponent = () => {
         </section>
         <section
           id="store-component-apps"
-          className="flex flex-column w-90 center pt8"
+          className="flex flex-column w-90 w-80-ns center pt8"
         >
-          <h2 className="c-emphasis t-heading-2">Store Component Apps</h2>
-          <ul className="t-body list">
+          <h2 className="c-base t-heading-3 fw3 mb0 mt4">Store Component Apps</h2>
+          <ul className="t-body list pl0 mt0">
             {storeComponents.map(component => (
               <li className="mv7 bb b--muted-2">
                 <p>
@@ -97,10 +104,10 @@ const FeaturesDisplay: FunctionComponent = () => {
         </section>
         <section
           id="store-pixel-apps"
-          className="flex flex-column w-90 center pt8"
+          className="flex flex-column w-90 w-80-ns center pt8"
         >
-          <h2 className="c-emphasis t-heading-2">Store Pixel Apps</h2>
-          <ul className="t-body list">
+          <h2 className="c-base t-heading-3 fw3 mb0 mt4">Store Pixel Apps</h2>
+          <ul className="t-body list pl0 mt0">
             {storePixel.map(app => (
               <li className="mv7 bb b--muted-2">
                 <p>{app.name}</p>
