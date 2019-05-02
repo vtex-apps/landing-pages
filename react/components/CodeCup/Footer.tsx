123047522c16aaa268c561d9df9eebcb7095c12e
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-05-01 22:15:33 -0300

    Adjustments to Footer layout

--- react/components/CodeCup/Footer.tsx
+++ react/components/CodeCup/Footer.tsx
@@ -3,17 +3,17 @@ import React, { FunctionComponent } from 'react'
 import Logo from '../../images/logo_codecup_03@2x.png'
 
 const Footer: FunctionComponent = () => (
-  <footer className="flex justify-around pa6">
-    <div className="flex flex-column">
-      <img src={Logo} />
-      <div className="flex justify-around">
+  <footer className="flex justify-around pa8">
+    <div className="flex flex-column w-25 justify-around">
+      <img src={Logo} className="w-70" />
+      <div className="flex justify-around mt7">
         <div>Twitter</div>
         <div>Facebook</div>
         <div>Instagram</div>
       </div>
     </div>
-    <div className="flex flex-column justify-around">
-      <ul className="flex list">
+    <div className="flex flex-column justify-around w-30 c-on-base">
+      <ul className="flex list justify-around">
         <li>vtex.com</li>
         <li>termos e condições</li>
         <li>careers</li>
