767222803605f6c672012efbef72a4c9571e33f4
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-05-03 13:37:53 -0300

    Add Carousel to Rio component for Code Cup landing

--- react/components/CodeCup/Rio.tsx
+++ react/components/CodeCup/Rio.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react'
-import { Carousel } from 'react-responsive-carousel'
+import Carousel from 'nuka-carousel'
 
 import CarouselImage1 from '../../images/06_foto_time.jpg'
 import CarouselImage2 from '../../images/05_foto_time.jpg'
@@ -9,40 +9,13 @@ import CarouselImage5 from '../../images/02_foto_rio.jpg'
 
 const Rio: FunctionComponent = () => (
   <section>
-    <Carousel>
-      <div>
-        <img src={CarouselImage1} />
-      </div>
-      <div>
-        <img src={CarouselImage2} />
-      </div>
-      <div>
-        <img src={CarouselImage3} />
-      </div>
-      <div>
-        <img src={CarouselImage4} />
-      </div>
-      <div>
-        <img src={CarouselImage5} />
-      </div>
+    <Carousel autoplay wrapAround withoutControls heightMode="first">
+      <img src={CarouselImage1} />
+      <img src={CarouselImage2} />
+      <img src={CarouselImage3} />
+      <img src={CarouselImage4} />
+      <img src={CarouselImage5} />
     </Carousel>
-    {/* <div className="flex flex-row-l flex-column items-center justify-center-s pl9-l ph5">
-      <p
-        className="t-heading-1 mb3 pt5 c-on-base self-baseline dn flex-l"
-        style={{ fontSize: `20rem` }}
-      >
-        rio
-      </p>
-      <p
-        className="t-heading-1 mb3 pa5 c-on-base self-baseline dn-l"
-        style={{ fontSize: `5rem` }}
-      >
-        rio
-      </p>
-      <p className="self-baseline t-heading-3 normal c-on-base">
-        cidade da maratona de programação da VTEX
-      </p>
-    </div> */}
   </section>
 )
 
