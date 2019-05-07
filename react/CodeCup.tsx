6c71ab3b6b613c483fec6d9fbcdfb1747906f895
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-05-07 08:44:26 -0300

    Add social metatags to Code Cup landing

--- react/CodeCup.tsx
+++ react/CodeCup.tsx
@@ -11,6 +11,8 @@ import VTEX from './components/CodeCup/VTEX'
 import FAQ from './components/CodeCup/FAQ'
 import Footer from './components/CodeCup/Footer'
 
+import HomeImage from './images/imagem_home.jpg'
+
 function initializeReactGA() {
   ReactGA.initialize('UA-139462697-1')
   ReactGA.pageview('/')
@@ -25,12 +27,31 @@ class Landing extends Component {
       <Fragment>
         <Helmet>
           <title>VTEX Code Cup</title>
-          <meta name="description" content="Coding competition from VTEX" />
+          <meta name="title" content="VTEX Code Cup" />
+          <meta name="description" content="The coding competition from VTEX" />
           <meta name="theme-color" content="#F71963" />
           <meta
             name="google-site-verification"
             content="w74PwlErxsMKe0Hp083-KEET2vwM2XcjzjiS4dZcThU"
           />
+          {/* <!-- Open Graph / Facebook --> */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://codecup.vtex.com/" />
+          <meta property="og:title" content="VTEX Code Cup" />
+          <meta
+            property="og:description"
+            content="The coding competition from VTEX"
+          />
+          <meta property="og:image" content={HomeImage} />
+          {/* <!-- Twitter --> */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://codecup.vtex.com/" />
+          <meta property="twitter:title" content="VTEX Code Cup" />
+          <meta
+            property="twitter:description"
+            content="The coding competition from VTEX"
+          />
+          <meta property="twitter:image" content={HomeImage} />
           <link rel="icon" href={favicon} />
         </Helmet>
         <main className="w-100 bg-white">
