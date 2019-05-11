0eb7885f3370d3ee097aaa21f310ad9b877159cc
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-05-11 18:56:51 -0300

    Add CodeCup first phase winners

--- react/components/CodeCup/ToTheSubscribed.tsx
+++ react/components/CodeCup/ToTheSubscribed.tsx
@@ -1,43 +1,135 @@
 import React from 'react'
 
+import First from '../../images/Tiago1.jpg'
+import Second from '../../images/Artur2.jpg'
+import Third from '../../images/Gabriel3.jpg'
+import Fourth from '../../images/Cezar4.jpg'
+import Fifth from '../../images/Ivan5.jpg'
+import Sixth from '../../images/Misael6.jpg'
+import Seventh from '../../images/Lucas7.jpg'
+import Eighth from '../../images/Artur8.png'
+import Nineth from '../../images/Luis9.jpg'
+import Tenth from '../../images/Lucas10.jpg'
+
 const ToTheSubscribed = () => (
   <section id="about" className="bg-white w-90 w-75-l center pv9">
-    <h1 className="c-emphasis t-heading-1">Para os inscritos</h1>
-    <h3 className="t-heading-3">
-      Muito obrigado por se inscrever no VTEX Code Cup 2019!
-    </h3>
-    <h4>Instruções para participação:</h4>
-    <h4>Plataforma</h4>
-    <p className="lh-copy">
-      O contest irá usar a plataforma{' '}
-      <a href="https://open.kattis.com">Kattis(https://open.kattis.com)</a> e
-      poderá ser feito através do link{' '}
-      <a href="https://open.kattis.com">https://vtexcc19.kattis.com/</a>.
-    </p>
-    <h4>Registro</h4>
-    <p className="lh-copy">
-      Para os candidatos que se inscreveram previamente pelo site
-      <a href="http://codecup.vtex.com">http://codecup.vtex.com</a>, devem ter
-      recebido um e-mail para registro da conta na plataforma Kattis. Caso você
-      ainda não tenha acesso ao Kattis ou não tenha recebido o email de
-      cadastro, será necessário se registrar em{' '}
-      <a href="https://open.kattis.com/login">https://open.kattis.com/login</a>.
-    </p>
-    <h4>Contest</h4>
-    <p className="lh-copy">
-      O contest irá ocorrer no dia{' '}
-      <strong>11/05 às 13hrs no horário de Brasília com duração de 3h30</strong>
-      . Para participar do contest você deverá logar com a conta associada ao
-      seu email de cadastro no Kattis. O resultado dos vencedores será
-      disponibilizado no site{' '}
-      <a href="http://codecup.vtex.com">http://codecup.vtex.com</a> em até 2hrs
-      após o término do contest.
-    </p>
-    <h4>Regulamento</h4>
-    <p className="lh-copy">
-      Pedimos que os competidores estejam atentos ao{' '}
-      <a href="/rules">regulamento</a> e as regras de qualificação.
-    </p>
+    <h1 className="c-emphasis t-heading-1">
+      Selecionados para a final no Rio de Janeiro
+    </h1>
+    <div className="flex flex-wrap justify-around">
+      <article className="bg-muted-1 mv3 ph6 pv5 w-40-l w-90 br3">
+        <div className="flex items-center link">
+          <div className="mr7">
+            <img src={First} />
+          </div>
+          <div>
+            <p className="t-heading-4 c-muted-5 c-emphasis">1º Lugar</p>
+            <p className="t-body c-muted-2">Tiago Gonçalves</p>
+          </div>
+        </div>
+      </article>
+      <article className="bg-muted-1 mv3 ph6 pv5 w-40-l w-90 br3">
+        <div className="flex items-center link">
+          <div className="mr7">
+            <img src={First} />
+          </div>
+          <div>
+            <p className="t-heading-4 c-muted-5 c-emphasis">2º Lugar</p>
+            <p className="t-body c-muted-2">Arthur Pratti Dadalto</p>
+          </div>
+        </div>
+      </article>
+      <article className="bg-muted-1 mv3 ph6 pv5 w-40-l w-90 br3">
+        <div className="flex items-center link">
+          <div className="mr7">
+            <img src={First} />
+          </div>
+          <div>
+            <p className="t-heading-4 c-muted-5 c-emphasis">3º Lugar</p>
+            <p className="t-body c-muted-2">Gabriel Pessoa</p>
+          </div>
+        </div>
+      </article>
+      <article className="bg-muted-1 mv3 ph6 pv5 w-40-l w-90 br3">
+        <div className="flex items-center link">
+          <div className="mr7">
+            <img src={First} />
+          </div>
+          <div>
+            <p className="t-heading-4 c-muted-5 c-emphasis">4º Lugar</p>
+            <p className="t-body c-muted-2">
+              Cezar Abadio Correia Guimarães Filho
+            </p>
+          </div>
+        </div>
+      </article>
+      <article className="bg-muted-1 mv3 ph6 pv5 w-40-l w-90 br3">
+        <div className="flex items-center link">
+          <div className="mr7">
+            <img src={First} />
+          </div>
+          <div>
+            <p className="t-heading-4 c-muted-5 c-emphasis">5º Lugar</p>
+            <p className="t-body c-muted-2">Ivan Henrique Costa Petrin</p>
+          </div>
+        </div>
+      </article>
+      <article className="bg-muted-1 mv3 ph6 pv5 w-40-l w-90 br3">
+        <div className="flex items-center link">
+          <div className="mr7">
+            <img src={First} />
+          </div>
+          <div>
+            <p className="t-heading-4 c-muted-5 c-emphasis">6º Lugar</p>
+            <p className="t-body c-muted-2">Misael Mateus Oliveira de Moraes</p>
+          </div>
+        </div>
+      </article>
+      <article className="bg-muted-1 mv3 ph6 pv5 w-40-l w-90 br3">
+        <div className="flex items-center link">
+          <div className="mr7">
+            <img src={First} />
+          </div>
+          <div>
+            <p className="t-heading-4 c-muted-5 c-emphasis">7º Lugar</p>
+            <p className="t-body c-muted-2">Lucas Soares Ferreira</p>
+          </div>
+        </div>
+      </article>
+      <article className="bg-muted-1 mv3 ph6 pv5 w-40-l w-90 br3">
+        <div className="flex items-center link">
+          <div className="mr7">
+            <img src={First} />
+          </div>
+          <div>
+            <p className="t-heading-4 c-muted-5 c-emphasis">8º Lugar</p>
+            <p className="t-body c-muted-2">Artur Nascimento</p>
+          </div>
+        </div>
+      </article>
+      <article className="bg-muted-1 mv3 ph6 pv5 w-40-l w-90 br3">
+        <div className="flex items-center link">
+          <div className="mr7">
+            <img src={First} />
+          </div>
+          <div>
+            <p className="t-heading-4 c-muted-5 c-emphasis">9º Lugar</p>
+            <p className="t-body c-muted-2">Luís Felipe Braga Gebrim Silva</p>
+          </div>
+        </div>
+      </article>
+      <article className="bg-muted-1 mv3 ph6 pv5 w-40-l w-90 br3">
+        <div className="flex items-center link">
+          <div className="mr7">
+            <img src={First} />
+          </div>
+          <div>
+            <p className="t-heading-4 c-muted-5 c-emphasis">10º Lugar</p>
+            <p className="t-body c-muted-2">Lucas Turci</p>
+          </div>
+        </div>
+      </article>
+    </div>
   </section>
 )
 
