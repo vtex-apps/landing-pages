5feacaa009e660bd4b79283dd18442d343cbea8c
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-05-21 15:27:26 -0300

    Design adjustments to IO Docs page

--- react/components/Docs/RendererPlaceHolder.tsx
+++ react/components/Docs/RendererPlaceHolder.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent } from 'react'
 import { Spinner } from 'vtex.styleguide'
 
 const RendererPlaceHolder: FunctionComponent = () => (
-  <div className="pa7 bg-base--inverted br3 flex justify-center items-center">
+  <div className="pa7 w-100 flex justify-center">
     <Spinner />
   </div>
 )
