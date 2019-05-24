86047d4122e1530f5af195d04254c19c9e83af4d
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-05-24 12:31:23 -0300

    Layout adjustment on TETRIX landing footer

--- react/components/Tetrix/Footer.tsx
+++ react/components/Tetrix/Footer.tsx
@@ -7,10 +7,10 @@ const Footer: FunctionComponent = () => (
     <div className="flex justify-between items-center w-90 center">
       <img src={Logo} alt="TETRIX" />
       <div className="flex justify-between">
-        <p>VTEX.COM</p>
-        <p>TERMOS E CONDIÇÕES</p>
-        <p>CAREERS</p>
-        <p>CONTACT US</p>
+        <p className="ph5">VTEX.COM</p>
+        <p className="ph5">TERMOS E CONDIÇÕES</p>
+        <p className="ph5">CAREERS</p>
+        <p className="ph5">CONTACT US</p>
       </div>
     </div>
   </footer>
