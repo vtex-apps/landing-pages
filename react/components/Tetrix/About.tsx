b2f38fa1a4b6c8899261b1cfb0bb9bf9b95f2c8a
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-05-24 11:47:28 -0300

    Overall layout updates to TETRIX landing

--- react/components/Tetrix/About.tsx
+++ react/components/Tetrix/About.tsx
@@ -9,28 +9,57 @@ import Icon6 from '../../images/icone6.png'
 import Icon7 from '../../images/icone7.png'
 import Icon8 from '../../images/icone8.png'
 
-const About: FunctionComponent = () => (
-  <section className="pa7 flex justify-around">
-    <div className="w-40">
-      <p className="t-heading-2 c-emphasis">O QUE É TETRIX?</p>
-      <p className="t-body lh-copy">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere aliquam
-        ipsa nihil vitae sint? Tempore exercitationem repudiandae blanditiis ut
-        quo iure recusandae, totam aut distinctio, possimus tenetur quam aliquam
-        ipsa.
-      </p>
-    </div>
-    <div className="flex justify-around flex-wrap">
-      <img src={Icon1} className="w-15 mh2 h4" />
-      <img src={Icon2} className="w-15 mh2 h4" />
-      <img src={Icon3} className="w-15 mh2 h4" />
-      <img src={Icon4} className="w-15 mh2 h4" />
-      <img src={Icon5} className="w-15 mh2 h4" />
-      <img src={Icon6} className="w-15 mh2 h4" />
-      <img src={Icon7} className="w-15 mh2 h4" />
-      <img src={Icon8} className="w-15 mh2 h4" />
-    </div>
-  </section>
-)
+const About: FunctionComponent = () => {
+  const iconClasses = 'w-25 pa5 flex flex-column items-center justify-center'
+
+  return (
+    <section className="pa7 flex justify-between items-center">
+      <div className="w-33">
+        <p className="t-heading-2 c-emphasis">O QUE É TETRIX?</p>
+        <p className="t-body lh-copy">
+          É um desafio que foi desenvolvido para testar e validar os diferentes
+          tipos de habilidades dos participantes, através de perguntas que
+          conectam múltiplos conhecimentos com um nível de complexidade
+          progressiva, direcionado a identificar diferentes perspectivas e
+          buscar solucionadores.
+        </p>
+      </div>
+      <div className="flex justify-around items-center flex-wrap w-60">
+        <div className={iconClasses}>
+          <img src={Icon1} />
+          <p className="c-emphasis tc">EVOLUÇÃO</p>
+        </div>
+        <div className={iconClasses}>
+          <img src={Icon2} />
+          <p className="tc">CONHECIMENTO TÉCNICO E DE MERCADO</p>
+        </div>
+        <div className={iconClasses}>
+          <img src={Icon3} />
+          <p className="c-emphasis tc">AUDÁCIA</p>
+        </div>
+        <div className={iconClasses}>
+          <img src={Icon4} />
+          <p className="tc">PENSAMENTO LÓGICO</p>
+        </div>
+        <div className={iconClasses}>
+          <img src={Icon5} />
+          <p className="tc">COLLECTIVE ADMIRATION</p>
+        </div>
+        <div className={iconClasses}>
+          <img src={Icon6} />
+          <p className="c-emphasis tc">CONEXÃO</p>
+        </div>
+        <div className={iconClasses}>
+          <img src={Icon7} />
+          <p className="tc">COMUNICAÇÃO</p>
+        </div>
+        <div className={iconClasses}>
+          <img src={Icon8} />
+          <p className="c-emphasis tc">REFERENCE VOICE</p>
+        </div>
+      </div>
+    </section>
+  )
+}
 
 export default About
