b15f4803c633f25eb8020064c7c88d597b902732
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-05-25 16:05:36 -0300

    Fix Footer for TETRIX landing

--- react/components/Tetrix/Footer.tsx
+++ react/components/Tetrix/Footer.tsx
@@ -5,7 +5,7 @@ import Logo from '../../images/tetrix_logo_dark.svg'
 const Footer: FunctionComponent = () => (
   <footer className="pa10-l pa7 bg-white">
     <div className="flex flex-row-l flex-column justify-between items-center-l w-90 center">
-      <img src={Logo} alt="TETRIX" className="w-90 center" />
+      <img src={Logo} alt="TETRIX" className="w-90 w-33-l center" />
       <div className="flex flex-row-l flex-column justify-between mt5 mt0-l">
         <p className="ph5-l">VTEX.COM</p>
         <p className="ph5-l">TERMOS E CONDIÇÕES</p>
