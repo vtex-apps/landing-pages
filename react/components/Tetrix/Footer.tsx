b9e6e72c6f17d2c096242e466f3667724d480e3c
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-05-25 15:17:45 -0300

    Overall layout tweaks to TETRIX landing page

--- react/components/Tetrix/Footer.tsx
+++ react/components/Tetrix/Footer.tsx
@@ -3,7 +3,7 @@ import React, { FunctionComponent } from 'react'
 import Logo from '../../images/tetrix_logo_dark.svg'
 
 const Footer: FunctionComponent = () => (
-  <footer className="pa10 bg-white" style={{ fontFamily: 'Space Mono' }}>
+  <footer className="pa10 bg-white">
     <div className="flex justify-between items-center w-90 center">
       <img src={Logo} alt="TETRIX" />
       <div className="flex justify-between">
