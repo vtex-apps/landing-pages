5ae7ef1ea9de73fca3960d655b9bc520eeefd315
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-05-29 20:49:54 -0300

    Minor update to Prizes from TETRIX landing

--- react/components/Tetrix/Prizes.tsx
+++ react/components/Tetrix/Prizes.tsx
@@ -24,71 +24,119 @@ const Prizes: FunctionComponent = () => {
           para conhecer tudo que existe sobre comércio global, guiado por uma
           das maiores empresas desse mundo.
         </p>
-        <p
-          className={`${
-            activeSlide === 0 ? 't-heading-1 c-emphasis' : 't-heading-2'
-          } pointer`}
-          onMouseOver={() => setActiveSlide(0)}
-          onClick={() => setActiveSlide(0)}
-        >
-          SANTIAGO
-        </p>
-        <p
-          className={`${
-            activeSlide === 1 ? 't-heading-1 c-emphasis' : 't-heading-2'
-          } pointer`}
-          onMouseOver={() => setActiveSlide(1)}
-          onClick={() => setActiveSlide(1)}
-        >
-          NOVA YORQUE
-        </p>
-        <p
-          className={`${
-            activeSlide === 2 ? 't-heading-1 c-emphasis' : 't-heading-2'
-          } pointer`}
-          onMouseOver={() => setActiveSlide(2)}
-          onClick={() => setActiveSlide(2)}
-        >
-          LONDRES
-        </p>
-        <p
-          className={`${
-            activeSlide === 3 ? 't-heading-1 c-emphasis' : 't-heading-2'
-          } pointer`}
-          onMouseOver={() => setActiveSlide(3)}
-          onClick={() => setActiveSlide(3)}
-        >
-          BERLIM
-        </p>
-        <p
-          className={`${
-            activeSlide === 4 ? 't-heading-1 c-emphasis' : 't-heading-2'
-          } pointer`}
-          onMouseOver={() => setActiveSlide(4)}
-          onClick={() => setActiveSlide(4)}
-        >
-          HONG KONG
-        </p>
-        <p
-          className={`${
-            activeSlide === 5 ? 't-heading-1 c-emphasis' : 't-heading-2'
-          } pointer`}
-          onMouseOver={() => setActiveSlide(5)}
-          onClick={() => setActiveSlide(5)}
-        >
-          SÃO PAULO
-        </p>
-        <p
-          className={`${
-            activeSlide === 6 ? 't-heading-1 c-emphasis' : 't-heading-2'
-          } pointer`}
-          onMouseOver={() => setActiveSlide(6)}
-          onClick={() => setActiveSlide(6)}
-        >
-          RIO DE JANEIRO
-        </p>
+        <div>
+          <p
+            className={`${activeSlide === 0 &&
+              't-heading-1-l'} pointer t-heading-2 c-emphasis`}
+            onMouseOver={() => setActiveSlide(0)}
+            onClick={() => setActiveSlide(0)}
+          >
+            SANTIAGO
+          </p>
+          <p className="dn-l">
+            E-commerce na América Latina - visão e desafios: identificar as
+            características do mercado e dos clientes latinos.
+          </p>
+        </div>
+        <div>
+          <p
+            className={`${activeSlide === 1 &&
+              't-heading-1-l'} pointer t-heading-2 c-emphasis`}
+            onMouseOver={() => setActiveSlide(1)}
+            onClick={() => setActiveSlide(1)}
+          >
+            NOVA YORQUE
+          </p>
+          <p className="dn-l">
+            Megalópole digital - networking e expansão de negócios em inovação:
+            colocar em prática habilidades de desenvolvimento de networking na
+            área de tecnologia.
+          </p>
+        </div>
+
+        <div>
+          <p
+            className={`${activeSlide === 2 &&
+              't-heading-1-l'} pointer t-heading-2 c-emphasis`}
+            onMouseOver={() => setActiveSlide(2)}
+            onClick={() => setActiveSlide(2)}
+          >
+            LONDRES
+          </p>
+          <p className="dn-l">
+            Fintechs - a inovação tecnológica revolucionando o mercado
+            financeiro global: entender como as fintechs revolucionaram o
+            mercado mundial e os desafios a seu desenvolvimento como, por
+            exemplo, os limites impostos por diferentes regulações feitas pelos
+            países.
+          </p>
+        </div>
+
+        <div>
+          <p
+            className={`${activeSlide === 3 &&
+              't-heading-1-l'} pointer t-heading-2 c-emphasis`}
+            onMouseOver={() => setActiveSlide(3)}
+            onClick={() => setActiveSlide(3)}
+          >
+            BERLIM
+          </p>
+          <p className="dn-l">
+            Inovação tecnológica: desenvolvimento do olhar mais afeto às
+            pesquisas, centros tecnológicos, universidades, para se antecipar
+            nos negócios digitais.
+          </p>
+        </div>
+
+        <div>
+          <p
+            className={`${activeSlide === 4 &&
+              't-heading-1-l'} pointer t-heading-2 c-emphasis`}
+            onMouseOver={() => setActiveSlide(4)}
+            onClick={() => setActiveSlide(4)}
+          >
+            HONG KONG
+          </p>
+          <p className="dn-l">
+            Visão para o futuro: novos mercados, diferentes culturas, conhecer
+            os desafios de empreender em uma cultura diferente da brasileira.
+          </p>
+        </div>
+
+        <div>
+          <p
+            className={`${activeSlide === 5 &&
+              't-heading-1-l'} pointer t-heading-2 c-emphasis`}
+            onMouseOver={() => setActiveSlide(5)}
+            onClick={() => setActiveSlide(5)}
+          >
+            SÃO PAULO
+          </p>
+          <p className="dn-l">
+            Centro de negócios do Brasil e da América Latina: conhecer a os
+            diferenciais da capital paulista no cenário do comércio eletrônico
+            brasileiro e reconhecer sua posição de importância no cenário do
+            e-commerce da América Latina.
+          </p>
+        </div>
+
+        <div>
+          <p
+            className={`${activeSlide === 6 &&
+              't-heading-1-l'} pointer t-heading-2 c-emphasis`}
+            onMouseOver={() => setActiveSlide(6)}
+            onClick={() => setActiveSlide(6)}
+          >
+            RIO DE JANEIRO
+          </p>
+          <p className="dn-l">
+            Negócios digitais na prática: conhecer e entender os desafios no
+            desenvolvimento e na programação da infraestrutura de negócios
+            digitais.
+          </p>
+        </div>
       </div>
-      <div className="w-50-l w-75 center">
+      <div className="w-50-l w-75 dn flex-l center">
         <Carousel heightMode="first" slideIndex={activeSlide} withoutControls>
           <div className="relative">
             <p className="z-2 pt6 absolute w-33 dn flex-l">
