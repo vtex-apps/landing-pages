f4928753500f9dcfd0ef6bff94f5e4976a2d7952
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-05-29 15:00:25 -0300

    Adjustments to HowItWorks section mobile layout

--- react/components/Tetrix/HowItWorks.tsx
+++ react/components/Tetrix/HowItWorks.tsx
@@ -1,17 +1,17 @@
 import React, { FunctionComponent } from 'react'
 
 const HowItWorks: FunctionComponent = () => (
-  <section className="pa7 mv8 flex justify-between items-center">
+  <section className="pa7 mv8 flex flex-column flex-row-l justify-between items-center">
     <div className="w-40-l">
       <p className="t-heading-1 c-emphasis">COMO FUNCIONA?</p>
-      <p>
+      <p className="lh-copy">
         O desafio possui 100 perguntas, divididas em 3 fases, sendo as duas
         primeiras online e a final presencial. Todos os inscritos terão acesso a
         primeira fase, e avançarão conforme o desempenho da fase anterior.
       </p>
     </div>
-    <div className="flex justify-between c-emphasis w-50-l">
-      <div className="w-30">
+    <div className="flex flex-column flex-row-l justify-between c-emphasis w-50-l w-90">
+      <div className="w-30-l mv5 mv0-l">
         <div className="b--emphasis ba pa5 tc">
           <p>FASE 1</p>
           <p className="t-heading-1 mv2">90</p>
@@ -21,7 +21,7 @@ const HowItWorks: FunctionComponent = () => (
           <p className="mt0">ONLINE</p>
         </div>
       </div>
-      <div className="w-30">
+      <div className="w-30-l mv5 mv0-l">
         <div className="b--emphasis ba pa5 tc">
           <p>FASE 2</p>
           <p className="t-heading-1 mv2">08</p>
@@ -31,7 +31,7 @@ const HowItWorks: FunctionComponent = () => (
           <p className="mt0">ONLINE</p>
         </div>
       </div>
-      <div className="w-30">
+      <div className="w-30-l mv5 mv0-l">
         <div className="b--emphasis ba pa5 tc">
           <p>FASE 3</p>
           <p className="t-heading-1 mv2">02</p>
