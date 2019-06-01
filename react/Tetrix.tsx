c02bb7848648ff212b5034e732f4af1b327c2fcc
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-06-01 14:46:09 -0300

    Add favicon and HongKong image for TETRIX

--- react/Tetrix.tsx
+++ react/Tetrix.tsx
@@ -8,37 +8,38 @@ import Prizes from './components/Tetrix/Prizes'
 import FAQ from './components/Tetrix/FAQ'
 import Footer from './components/Tetrix/Footer'
 import HowItWorks from './components/Tetrix/HowItWorks'
+import favicon from './images/faviconTetrix.png'
 
 const Tetrix: FunctionComponent = () => (
   <Fragment>
     <Helmet>
-      <title>Tetrix - O Desafio</title>
+      <title>Tetrix - O Desafio Universitário</title>
       <meta name="title" content="Tetrix" />
-      {/* <meta name="description" content="The coding competition from VTEX" /> */}
+      <meta name="description" content="O desafio universitário da VTEX" />
       <meta name="theme-color" content="#F71963" />
+      <link rel="icon" href={favicon} />
       {/* <meta
         name="google-site-verification"
         content="w74PwlErxsMKe0Hp083-KEET2vwM2XcjzjiS4dZcThU"
       /> */}
       {/* <!-- Open Graph / Facebook --> */}
-      {/* <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://codecup.vtex.com/" />
-      <meta property="og:title" content="VTEX Code Cup" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://tetrix.vtex.com/" />
+      <meta property="og:title" content="Tetrix" />
       <meta
         property="og:description"
-        content="The coding competition from VTEX"
+        content="O desafio universitário da VTEX"
       />
-      <meta property="og:image" content={HomeImage} /> */}
+      {/* <meta property="og:image" content={HomeImage} /> */}
       {/* <!-- Twitter --> */}
-      {/* <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://codecup.vtex.com/" />
-      <meta property="twitter:title" content="VTEX Code Cup" />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://tetrix.vtex.com/" />
+      <meta property="twitter:title" content="Tetrix" />
       <meta
         property="twitter:description"
-        content="The coding competition from VTEX"
+        content="O desafio universitário da VTEX"
       />
-      <meta property="twitter:image" content={HomeImage} /> */}
-      {/* <link rel="icon" href={favicon} /> */}
+      {/* <meta property="twitter:image" content={HomeImage} /> */}
     </Helmet>
     <main className="w-100 bg-white">
       <div className="w-90-l center">
