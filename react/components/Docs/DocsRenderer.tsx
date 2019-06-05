99f0582153e3e4f0e813d613be6722c6294ef2a8
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-06-05 18:37:39 -0300

    Add smooth scrolling to Docs

--- react/components/Docs/DocsRenderer.tsx
+++ react/components/Docs/DocsRenderer.tsx
@@ -2,14 +2,31 @@ import React, { FunctionComponent, Fragment } from 'react'
 import ReactHtmlParser from 'react-html-parser'
 import { graphql, compose } from 'react-apollo'
 import { branch, renderComponent } from 'recompose'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 import RendererPlaceHolder from './RendererPlaceHolder'
 import EmptyDocs from './EmptyDocs'
 
 import * as RepoDocs from '../../graphql/getDocs.graphql'
 
+function transform(node: any) {
+  if (
+    node.type === 'tag' &&
+    node.name === 'a' &&
+    node.attribs.href[0] === '#'
+  ) {
+    return (
+      <AnchorLink offset={() => 80} href={node.attribs.href}>
+        {node.children[0].data}
+      </AnchorLink>
+    )
+  }
+}
+
 const DocsRenderer: FunctionComponent<any> = ({ docsQuery }) => (
-  <Fragment>{ReactHtmlParser(docsQuery.getDocs.htmlDocs)}</Fragment>
+  <Fragment>
+    {ReactHtmlParser(docsQuery.getDocs.htmlDocs, { transform })}
+  </Fragment>
 )
 
 export default compose(
