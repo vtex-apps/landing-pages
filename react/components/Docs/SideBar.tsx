dafb5866680692e8b98870d9661e845eb4ac4b1a
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-06-17 19:06:26 -0300

    Tweaks to SideBar component for Docs page

--- react/components/Docs/SideBar.tsx
+++ react/components/Docs/SideBar.tsx
@@ -10,17 +10,25 @@ interface Props {
 
 const SideBar: FunctionComponent<Props> = ({ appList }) => (
   <ul className="pa7 mt0 list br b--muted-5">
-    {appList.map(app => (
-      <li className="pv3 link" key={app.name}>
-        <Link to={`/docs?app=${app.name}&file=README.md`}>{app.name}</Link>
-        {app.files.length > 1 &&
-          app.files.map(file => (
-            <li className="pv3 link" key={app.name + file}>
-              <Link to={`/docs?app=${app.name}&file=${file}`}>{file}</Link>
-            </li>
-          ))}
-      </li>
-    ))}
+    {appList.map(app => {
+      const [vendor, name] = app.name.split('.')
+      return (
+        <li className="pv3 link" key={app.name}>
+          <Link to={`/docs?app=${app.name}&file=README.md`}>
+            {name.replace('-', ' ').toLocaleUpperCase()}
+          </Link>
+          {app.files.length > 1 && (
+            <ul className="pv3">
+              {app.files.map(file => (
+                <li className="pv3 link" key={app.name + file}>
+                  <Link to={`/docs?app=${app.name}&file=${file}`}>{file}</Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </li>
+      )
+    })}
   </ul>
 )
 
