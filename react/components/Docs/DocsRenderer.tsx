c0cca527964fb3dd8170f2ed480713897f11c4ca
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-06-17 18:06:47 -0300

    Bring back `remark` to Docs landing

--- react/components/Docs/DocsRenderer.tsx
+++ react/components/Docs/DocsRenderer.tsx
@@ -2,39 +2,11 @@ import React, { FunctionComponent, Fragment } from 'react'
 import ReactHtmlParser from 'react-html-parser'
 // import { graphql, compose } from 'react-apollo'
 // import { branch, renderComponent } from 'recompose'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { coy } from 'react-syntax-highlighter/dist/styles/prism'
 
 // import Skeleton from './Skeleton'
 // import EmptyDocs from './EmptyDocs'
 
 // import * as RepoDocs from '../../graphql/getDocs.graphql'
-
-function transform(node: any) {
-  if (
-    node.type === 'tag' &&
-    node.name === 'a' &&
-    node.attribs.href[0] === '#'
-  ) {
-    return (
-      <AnchorLink offset={() => 80} href={node.attribs.href}>
-        {node.children[0].data}
-      </AnchorLink>
-    )
-  } else if (
-    node.type === 'tag' &&
-    node.name === 'code' &&
-    node.parent &&
-    node.parent.name === 'pre'
-  ) {
-    return (
-      <SyntaxHighlighter language="javascript" style={coy}>
-        {node.children[0].data}
-      </SyntaxHighlighter>
-    )
-  }
-}
 
 const DocsRenderer: FunctionComponent<any> = () => (
   // <Fragment>
