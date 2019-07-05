21542a355c2aa3cc7915a52d278dae182b2414af
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-07-05 11:38:44 -0300

    Visual changes to Results component

--- react/components/CodeCup/Results.tsx
+++ react/components/CodeCup/Results.tsx
@@ -1,45 +1,45 @@
 import React from 'react'
 
 import First from '../../images/CodeCupResults1.jpg'
-import Second from '../../images/CodeCupResults2.jpg'
-import Third from '../../images/CodeCupResults3.jpg'
+import Third from '../../images/CodeCupResults2.jpg'
+import Second from '../../images/CodeCupResults3.jpg'
 
 const Results = () => (
   <section id="about" className="bg-white w-90 w-75-l center pv9">
     <h1 className="c-emphasis t-heading-1">
       Resultados VTEX Code Cup 2019
     </h1>
-    <div className="flex flex-column justify-around">
+    <div className="flex flex-column">
       <article className="bg-muted-1 mv3 ph6 pv5 w-90 br3">
-        <div className="flex items-center link">
+        <div className="items-center link flex">
           <div className="mr7">
-            <img src={First} className="h4 w4" />
+            <img src={First} className="vw-50 br2" />
           </div>
-          <div>
+          <div className="flex flex-column items-center">
             <p className="t-heading-4 c-muted-5 c-emphasis">1º Lugar</p>
             <p className="t-body c-muted-2">Arthur Nascimento</p>
           </div>
         </div>
       </article>
       <article className="bg-muted-1 mv3 ph6 pv5 w-90 br3">
-        <div className="flex items-center link">
+        <div className="items-center link flex">
           <div className="mr7">
-            <img src={Second} className="h4 w4" />
+            <img src={Second} className="vw-50 br2" />
           </div>
-          <div>
+          <div className="flex flex-column items-center">
             <p className="t-heading-4 c-muted-5 c-emphasis">2º Lugar</p>
-            <p className="t-body c-muted-2">Arthur Pratti Dadalto</p>
+            <p className="t-body c-muted-2">Tiago Gonçalves</p>
           </div>
         </div>
       </article>
       <article className="bg-muted-1 mv3 ph6 pv5 w-90 br3">
-        <div className="flex items-center link">
+        <div className="items-center link flex">
           <div className="mr7">
-            <img src={Third} className="h4 w4" />
+            <img src={Third} className="vw-50 br2" />
           </div>
-          <div>
-            <p className="t-heading-4 c-muted-5 c-emphasis">3º Lugar</p>
-            <p className="t-body c-muted-2">Gabriel Pessoa</p>
+          <div className="flex flex-column items-center">
+            <p className="t-heading-4 c-muted-5 c-emphasis center">3º Lugar</p>
+            <p className="t-body c-muted-2 center">Arthur Pratti Dadalto</p>
           </div>
         </div>
       </article>
